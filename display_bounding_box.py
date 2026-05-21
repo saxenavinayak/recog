c5f3707f01fe8942c158a20c617dcd7d2e4b6bb6
Vinayak Saxena <saxenavinayak12@gmail.com>
2026-05-20 22:21:55 -0400

    remove unused code

--- display_bounding_box.py
+++ display_bounding_box.py
@@ -13,81 +13,21 @@ from helpers.db_connector import postgress_connection
 from pgvector.psycopg2 import register_vector
 
 import pandas as pd
-# np.float = float    
-# np.int = int   #module 'numpy' has no attribute 'int'
-# np.object = object    #module 'numpy' has no attribute 'object'
-# np.bool = bool    #module 'numpy' has no attribute 'bool'
-
-# app = FaceAnalysis(providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
-# app.prepare(ctx_id=0, det_size=(640, 640))
-# register_heif_opener()
-
-# img = cv2.imread("/mnt/e/whyyy/AAGM9623.JPG")
-# faces = app.get(img)
-# for face in faces:
-#     bbox_data=face.bbox.astype(np.int)
-
-#     start_point = (int(bbox_data[0]), int(bbox_data[1]))  # (2011, 738)
-#     end_point   = (int(bbox_data[2]), int(bbox_data[3]))  # (2306, 1091)
-    
-#     cv2.rectangle(img, start_point, end_point, (0, 255, 0), 2)
-#     cv2.imwrite('output_bounded.jpg', img)
-
-
 
 
 # get medoid image (get the image which has the embedding with the higest average cosine similarity to all other embeddings)
-# Algo
-"""
-a = [
-    ("image1", "x1,y1, x2,y2", "tensor", label)
-    ("image2", "x1,y1, x2,y2", "tensor", label)
-    ("image3", "x1,y1, x2,y2", "tensor", label)
-    ("image4", "x1,y1, x2,y2", "tensor", label)
-    ("image5", "x1,y1, x2,y2", "tensor", label)
-]
-avg_cosine_images = []
-for each image in label:
-    current_candidate_tensor = image.tensor
-    simil = []
-    for each image2 in label:
-        s = cosine.simil(current_candidate_tensor, image2.tensor)
-        simil.append(s)
-    average = mean(simil)
-    avg_cosine_images.append((image, average))
-best_candidate_image = max(avg_cosine_images, key=average)        
-"""
-
-# img = cv2.imread("/mnt/e/whyyy/JAXW0072.JPG")
-
-
-
-
-# bbox_data = [2441.0, 1693.0, 2639.0, 1980.0]
-
-# # # Map them correctly by extracting the X and Y coordinates
-# # # Remember: OpenCV wants (X, Y)
-# start_point = (int(bbox_data[0]), int(bbox_data[1]))  # (x1, y1) -> (750, 2011)
-# end_point = (int(bbox_data[2]), int(bbox_data[3]))
-# cv2.rectangle(img, start_point, end_point, (0, 255, 0), 2)
-# cv2.imwrite('output_bounded.jpg', img)\
-
-
-
-
-
 
 
 conn = postgress_connection()
 register_vector(conn)
 cursor = conn.cursor()
 
-cursor.execute("SELECT id, image_name, norm_embedded_tensor, bounding_box, image_labels FROM photo_analysis WHERE image_labels=1")
+cursor.execute("SELECT id, image_name, norm_embedded_tensor, bounding_box, image_labels FROM photo_analysis WHERE image_labels=44")
 rows = cursor.fetchall()
 
 df = pd.DataFrame(rows, columns=["id", "image_name", "norm_embedded_tensor", "bounding_box", "image_labels"])
 
-avg_cosine_images = np.array([])
+avg_cosine_images = []
 for row in df.itertuples():
     current_candidate_tensor = row.norm_embedded_tensor
 
@@ -97,4 +37,17 @@ for row in df.itertuples():
         similarity = float(np.dot(current_candidate_tensor, tensor))
         simil = np.append(simil, similarity)
     print(f"The average cosine similarity score for image {row.image_name} is {np.mean(simil)}")
+    avg_cosine_images.append(
+        (row.image_name, np.mean(simil), row.bounding_box)
+        )
 
+best = max(avg_cosine_images, key=lambda x: x[1])
+print(best)
+
+img = cv2.imread(best[0])
+bbox_data = best[2]
+print(bbox_data)
+start_point = (int(bbox_data[0]), int(bbox_data[1]))  # (x1, y1) -> (750, 2011)
+end_point = (int(bbox_data[2]), int(bbox_data[3]))
+cv2.rectangle(img, start_point, end_point, (0, 255, 0), 2)
+cv2.imwrite('output_bounded.jpg', img)
